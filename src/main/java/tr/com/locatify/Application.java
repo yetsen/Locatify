5304cfb5d14400a9e0f3c5e4835faa582fdd6b1b
Yunus <yunusemretekin1@gmail.com>
2016-03-13 02:46:24 +0200

    Klasik war paketi üretmek için SpringBootServletInitializer  extend edildi

--- src/main/java/tr/com/locatify/Application.java
+++ src/main/java/tr/com/locatify/Application.java
@@ -2,6 +2,7 @@ package tr.com.locatify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 
 import java.util.Arrays;
@@ -11,7 +12,9 @@ import java.util.Arrays;
  */
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
     }
